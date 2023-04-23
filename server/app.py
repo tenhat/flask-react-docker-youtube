e6c141e37ab9c7cd6575e4a677a08f125f30213e
tenjuhattori <tenjuhattori@gmail.com>
2023-04-23 21:31:55 +0900

    初期コミット

---
+++ server/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return("Hello Chris Hey I'm on the web!aaa")
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
