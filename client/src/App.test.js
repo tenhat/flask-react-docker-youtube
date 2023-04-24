473d381d0557e4a4c55c72b367a48d0d62443a1b
tenjuhattori <tenjuhattori@gmail.com>
2023-04-24 21:54:30 +0900

    test: jestでfetchのテスト追加

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,34 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { act, renderHook } from '@testing-library/react-hooks';
+import { useEffect } from 'react';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('useEffect fetch', () => {
+  it('should call fetch and set state with data', async () => {
+    const mockData = { name: 'Test Data' };
+    const mockFetch = jest.fn().mockResolvedValue({
+      status: 200,
+      json: () => Promise.resolve(mockData),
+    });
+
+    // テスト内で使用するurlを設定し、useEffectをレンダリングします。
+    const url = "/api";
+    let setState = jest.fn();
+    await act(async () => {
+      renderHook(() => (
+        useEffect(() => {
+          fetch(url).then(res => {
+            if (res.status === 200) {
+              return res.json();
+            }
+          }).then(data => {
+            setState(data);
+          }).catch(err => console.log(err)); // エラー処理の追加
+        }, [])
+      ));
+    });
+
+    expect(mockFetch).toHaveBeenCalledWith(url);
+
+    // setStateがデータで呼ばれたことを確認します。
+    expect(setState).toHaveBeenCalledWith(mockData);
+  });
 });
